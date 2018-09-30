88ff1fb51e1332692463bd650d9bd81c650be5e7
stereobooster <stereobooster@gmail.com>
2018-09-30 10:53:25 +0200

    remove 'classical' approach to make code base smaller

--- src/redux.js
+++ src/redux.js
@@ -2,7 +2,7 @@
 // you don't do this in typical application, this is for the demonstration.
 // Pick either FSM or "classic":
 
-// Classic Redux
+// Classic Redux see https://github.com/stereobooster/react-fsm-example/tree/post-1
 // export * from "src/redux-classic/reducers";
 // export * from "src/redux-classic/store";
 // export * from "src/redux-classic/state";
